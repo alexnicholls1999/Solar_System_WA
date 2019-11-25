f7d73dacef739476407d6c400598ddf00449eaf1
alexnicholls1999 <azini@live.co.uk>
2019-11-25 21:41:45 +0000

    Mk 1 Rocket is launched

--- launch.js
+++ launch.js
@@ -1,18 +1,42 @@
-
-window.onload = function() {
+window.onload = function(){
     const path = {
         curviness: 0,
-        values: [{x: 0, y:0}, {x: 0, y: -500}]
+        values: [{x: 0, y:0}, {x: 0, y: 500}]
    }
-   
-   document.getElementById("launch").addEventListener("click", e=>{
-       document.getElementById("launch");
-       const tween = new TimelineLite();
-       tween.add(
-           TweenLite.to("#rocket", 2, {
-               bezier: path
-           })
-           );
-   });
+   document.getElementById("#rocket").style()
+   const tween = new TimelineLite();
+           tween.add(
+               TweenLite.to("#rocket", 4, {
+                   bezier: path
+               })
+           )
+}
+
+function landing() {
+    const path = {
+        curviness: 0,
+        values: [{x: 0, y:0}, {x: 0, y: 500}]
+   }
+   const tween = new TimelineLite();
+           tween.add(
+               TweenLite.to("#rocket", 4, {
+                   bezier: path
+               })
+           )
+
+}
+
+function launch(){
+    const path = {
+        curviness: 0, 
+        values: [{x: 0, y:0}, {x: 0, y: -300}]
+    }
+        const tween = new TimelineLite();
+        tween.add(
+            TweenLite.to("#rocket", 4, {
+                bezier: path
+            })
+        )
+
 }
 
