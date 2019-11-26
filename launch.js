d01551960fe6bf848de054808a4a6727b490d320
alexnicholls1999 <azini@live.co.uk>
2019-11-26 11:26:28 +0000

    animation is added to landing animation

--- launch.js
+++ launch.js
@@ -3,7 +3,8 @@ window.onload = function(){
         curviness: 0,
         values: [{x: 0, y:0}, {x: 0, y: 500}]
    }
-   document.getElementById("#rocket").style()
+
+
    const tween = new TimelineLite();
            tween.add(
                TweenLite.to("#rocket", 4, {
@@ -12,31 +13,35 @@ window.onload = function(){
            )
 }
 
-function landing() {
-    const path = {
-        curviness: 0,
-        values: [{x: 0, y:0}, {x: 0, y: 500}]
-   }
-   const tween = new TimelineLite();
-           tween.add(
-               TweenLite.to("#rocket", 4, {
-                   bezier: path
-               })
-           )
+// function landing() {
+//     const path = {
+//         curviness: 0,
+//         values: [{x: 0, y:0}, {x: 0, y: 500}]
+//    }
 
-}
+
+//    const tween = new TimelineLite();
+//            tween.add(
+//                TweenLite.to("#rocket", 4, {
+//                    bezier: path
+//                })
+//            )
+
+// }
+
 
 function launch(){
     const path = {
         curviness: 0, 
         values: [{x: 0, y:0}, {x: 0, y: -300}]
     }
-        const tween = new TimelineLite();
-        tween.add(
-            TweenLite.to("#rocket", 4, {
-                bezier: path
-            })
-        )
+        document.getElementById('launch').style.animation = "thrust 1.5s steps(2) 3";  
 
+            const tween = new TimelineLite();
+            tween.add(
+                TweenLite.to("#rocket", 4, {
+                    bezier: path
+                })
+            )
 }
 
