bbfda16bf780e4e207f9249441abb6205b6288ce
alexnicholls1999 <azini@live.co.uk>
2019-11-28 09:13:07 +0000

    Added Earth and Mars surface with beta solar system

--- launch.js
+++ launch.js
@@ -1,16 +1,24 @@
 window.onload = function(){
     const path = {
         curviness: 0,
-        values: [{x: 0, y:0}, {x: 0, y: 500}]
+        values: [{x: 0, y:0}, {x: 0, y: 450}]
    }
 
-
-   const tween = new TimelineLite();
+//    document.getElementById('rocket').style.backgroundPosition = "216px  270px";  
+    const tween = new TimelineLite({onComplete: stopSpriteSheet});
+    // const tween = new TimelineLite();
+//    document.getElementById('rocket').removeProperty(marginTop);
+//    document.getElementById('rocket').style.animation = "thrust 2s steps(2) 2"; 
+//    document.getElementById('rocket').style.marginTop = "-350px";  
            tween.add(
                TweenLite.to("#rocket", 4, {
                    bezier: path
-               })
+               })  
            )
+}
+
+function stopSpriteSheet(){
+    document.getElementById('rocket').classList.remove("launchAnimation");
 }
 
 // function landing() {
@@ -19,6 +27,7 @@ window.onload = function(){
 //         values: [{x: 0, y:0}, {x: 0, y: 500}]
 //    }
 
+//    document.getElementById('rocket').style.backgroundPosition = "-250px  0px";  
 
 //    const tween = new TimelineLite();
 //            tween.add(
@@ -33,15 +42,24 @@ window.onload = function(){
 function launch(){
     const path = {
         curviness: 0, 
-        values: [{x: 0, y:0}, {x: 0, y: -300}]
+        values: [{x: 0, y:0}, {x: 0, y: -1000}]
     }
-        document.getElementById('launch').style.animation = "thrust 1.5s steps(2) 3";  
+    
 
-            const tween = new TimelineLite();
-            tween.add(
-                TweenLite.to("#rocket", 4, {
-                    bezier: path
-                })
-            )
+    document.getElementById('rocket').classList.add("launchAnimation");
+    // document.getElementById('rocket').style.backgroundPosition = "-250px  0px";
+    // document.getElementById('rocket').style.animation = "thrust 2s steps(2) 2";  
+
+    const tween = new TimelineLite();
+    tween.add(
+        TweenLite.to("#rocket", 6, {
+        bezier: path
+        })
+    )
+
+    setTimeout (function(){
+        window.location.href = 'index.html';
+        // window.open('rocketlanding.html');
+    }, 3000);
 }
 
