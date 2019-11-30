e1089999cd7c29e723e91d5f855f5eedf0f389ce
alexnicholls1999 <azini@live.co.uk>
2019-11-30 14:48:24 +0000

    add new rocket

--- launch.js
+++ launch.js
@@ -1,9 +1,11 @@
+let launch_sound = new Audio('Rocket-Thrust.mp3');
+
 window.onload = function(){
 
     document.getElementById("rocket").classList.add('launchAnimation');
     const path = {
         curviness: 0,
-        values: [{x: 0, y:0}, {x: 0, y: 450}]
+        values: [{x: 0, y:0}, {x: 0, y: 850}]
    }
 
 //    document.getElementById('rocket').style.backgroundPosition = "216px  270px";  
@@ -11,12 +13,13 @@ window.onload = function(){
     // const tween = new TimelineLite();
 //    document.getElementById('rocket').removeProperty(marginTop);
 //    document.getElementById('rocket').style.animation = "thrust 2s steps(2) 2"; 
-//    document.getElementById('rocket').style.marginTop = "-350px";  
+
            tween.add(
                TweenLite.to("#rocket", 4, {
                    bezier: path
                })  
            )
+
 }
 
 
@@ -48,6 +51,7 @@ function launch(){
         values: [{x: 0, y:0}, {x: 0, y: -1000}]
     }
     
+    launch_sound.play();
 
     document.getElementById('rocket').classList.add("launchAnimation");
     // document.getElementById('rocket').style.backgroundPosition = "-250px  0px";
